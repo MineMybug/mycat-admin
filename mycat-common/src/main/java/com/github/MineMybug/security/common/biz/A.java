bb070d1f270f77dcf0f64ede2b09bff8c049de00
MineMybug <18360577760@163.com>
2018-10-22 19:16:30 +0800

    1.添加 公用工程common

---
+++ mycat-common/src/main/java/com/github/MineMybug/security/common/biz/A.java
@@ -0,0 +1,16 @@
+/**   
+* <p>Description: </p>  
+* <p>Copyright: Copyright (c) 2017</p>    
+* @date 2018年10月22日  
+* @version 1.0  
+*/
+package com.github.MineMybug.security.common.biz;
+
+/**   
+* <p>Description: </p>  
+* @author ruanhang  
+* @date 2018年10月22日  
+*/
+public class A {
+
+}
