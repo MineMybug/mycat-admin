8f69e41a1aaa5f2acde986d56719803cbd138852
MineMybug <18360577760@163.com>
2018-10-29 19:54:18 +0800

    1.修改相应配置

--- mycat-consumer/src/main/java/com/github/MineMybug/security/ConsumerApplication.java
+++ mycat-consumer/src/main/java/com/github/MineMybug/security/ConsumerApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 /**   
 * <p>Description: </p>  
